0faa7307db911815d118fd146b24cf89352725f7
Clacson <cookiellacson@gmail.com>
2016-10-17 21:13:48 +0100

    second task done, /address route completed

--- controller.rb
+++ controller.rb
@@ -7,3 +7,14 @@ require_relative('./models/hwk')
 get '/' do
   erb(:about_me)
 end
+
+get '/address' do
+  content_type(:json)
+  results = {
+    address: '3 ARGYLE HOUSE',
+    building: 'CODEBASE',
+    postcode: 'e13 zqf',
+    phone: '0131558030'
+  }
+  return results.to_json
+end
